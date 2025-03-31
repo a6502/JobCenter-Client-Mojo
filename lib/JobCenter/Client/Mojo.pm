a70d4ef756f2efa71ef78b7c875fdb462d80ea42
Michael Green <green@strato.de>
2025-03-31 13:29:11 +0200

    bump version number

--- lib/JobCenter/Client/Mojo.pm
+++ lib/JobCenter/Client/Mojo.pm
@@ -1,7 +1,7 @@
 package JobCenter::Client::Mojo;
 use Mojo::Base 'Mojo::EventEmitter';
 
-our $VERSION = '0.46'; # VERSION
+our $VERSION = '0.47'; # VERSION
 
 #
 # Mojo's default reactor uses EV, and EV does not play nice with signals
